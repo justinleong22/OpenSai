b6113936dee718c76270c60c23ff0f6d03656144
Justin Leong <117539348+justinleong22@users.noreply.github.com>
2026-04-13 16:38:52 -0700

    hw0

---
+++ homework/hw0/CMakeFiles/hw0-viz.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/justinleong/Documents/OpenSai/cs225a/bin/hw0/hw0-viz"
+  "/Users/justinleong/Documents/OpenSai/cs225a/bin/hw0/hw0-viz.pdb"
+  "CMakeFiles/hw0-viz.dir/hw0-viz.cpp.o"
+  "CMakeFiles/hw0-viz.dir/hw0-viz.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw0-viz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ homework/hw0/CMakeFiles/hw0-viz.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hw0-viz.
+# This may be replaced when dependencies are built.
